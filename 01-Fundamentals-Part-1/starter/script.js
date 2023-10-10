b8a0a612529bc3af740116852be2bc02c02ac10f
Kaushith_Gowda_M <koushith7@gmail.com>
2023-10-10 18:02:11 +0530

    8.Linking with JavaScript File
    Two ways of linking a script file demostrated

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+// Two ways of linking a script file
+// 1. Inline script
+// 2. External script
+
+let j = 'amazing';
+if (j === 'amazing') alert('JS is from external script');
+
+console.log(40 + 8 + 23 - 10, ": external script");
