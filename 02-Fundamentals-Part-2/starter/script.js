ae474c884205be0d98f214bfbeba3db9d938ab5d
Kaushith_Gowda_M <koushith7@gmail.com>
2023-10-12 19:09:50 +0530

    Use strict

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+// Strict Mode
+'use strict'
+// It forbidds us to do certain things
+// Gives us errors
+
+let hasDriversLincense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLincense) console.log('I can drive');
