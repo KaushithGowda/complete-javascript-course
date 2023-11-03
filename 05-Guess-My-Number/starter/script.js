44f7322e9d867dbf631bd7741287b694962ffebb
Kaushith_Gowda_M <koushith7@gmail.com>
2023-11-03 17:55:48 +0530

    Implenting the game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /*
+// 72. Selecting and Manipulating Elements
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = "Js is here!";
@@ -10,10 +11,40 @@ document.querySelector('.score').textContent = 13;
 document.querySelector('.guess').value = 10;
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+// 73. Handling click events
 document.querySelector('.check').addEventListener('click', () => {
     const guess = document.querySelector('.guess').value;
-    console.log(guess);
+
     if (!guess) {
         document.querySelector('.message').textContent = 'Enter a number⚠️';
     }
-})
+
+    // 74. Implenting the game logic
+    if (Number(guess) === secretNumber) {
+        document.querySelector('.message').textContent = 'You are goddamn right ✅';
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Think a smaller number 📉';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost sucker!';
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Think a bigger number 📈';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost sucker!';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
