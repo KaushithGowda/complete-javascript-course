a781862e688d59fec4659401dd68126d68fce4b4
Kaushith_Gowda_M <koushith7@gmail.com>
2023-11-03 13:51:39 +0530

    Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = "Js is here!";
+
+document.querySelector('.score').textContent = 13;
+
+document.querySelector('.guess').value = 10;
