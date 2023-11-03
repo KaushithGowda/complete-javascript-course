0bd998ee428fabb643090c21252a1c4db6951821
Kaushith_Gowda_M <koushith7@gmail.com>
2023-11-03 16:33:01 +0530

    Handling the click events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = "Js is here!";
@@ -7,3 +8,12 @@ document.querySelector('.message').textContent = "Js is here!";
 document.querySelector('.score').textContent = 13;
 
 document.querySelector('.guess').value = 10;
+*/
+
+document.querySelector('.check').addEventListener('click', () => {
+    const guess = document.querySelector('.guess').value;
+    console.log(guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = 'Enter a number⚠️';
+    }
+})
