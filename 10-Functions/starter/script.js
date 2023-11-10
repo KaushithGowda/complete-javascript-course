6b3cc1fd08fb3452665b1c308c39391d2a421927
Kaushith_Gowda_M <koushith7@gmail.com>
2023-11-10 17:49:49 +0530

    Default Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// 128. DEFAULT PARAMETERS
+
+const bookings = [];
+
+// ES6
+// The order of placing arguments must be appropriate
+// const createBooking = function (flightNum, price = 10 * numPassengeres, numPassengeres = 1) { // This is an inappropriate other
+
+const createBooking = function (flightNum, numPassengeres = 1, price = 10 * numPassengeres) {
+    // ES5
+    // numPassengeres = numPassengeres || 1;
+    // price = price || 10;
+
+    const booking = {
+        flightNum,
+        numPassengeres,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('FN009');
+createBooking('FN009', 10, 100);
+// We have to pass all arguments if one is dependent on other else calculations will get fucked up
+createBooking('FN009', 1000);
