813839eb7002da2a870a8180f1a3328b68d13c7e
Kaushith_Gowda_M <koushith7@gmail.com>
2023-11-14 17:18:34 +0530

    Constructor functions and the new Operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// 207. Constructor functions and the new Operator
+
+const User = function (name, phone) {
+    this.name = name;
+    this.phone = phone;
+}
+
+// 4 steps in creating a function using new Operator
+// 1. New {} (obj) is created
+// 2. function is called. this keyword points to {} (obj)
+// 3. {} (obj) is linked to prototype
+// 4. function automatically returns {}
+
+const bob = new User('bob', 9998877665);
+console.log(bob);
+console.log(bob instanceof User); // true
